5ad0bde9999e2b8b60a862637dcf7b429085a8b7
Edmund Tian <edmundtian@ezt.local>
2022-10-28 14:26:01 -0400

    fixes

--- src/constants.ts
+++ src/constants.ts
@@ -10,22 +10,15 @@
 
 
 // TODO: set this to 'prod' if you want to work with real data. Keep this as 'sandbox' if you want to work with sandbox data.
-// export const ENVIRONMENT = 'production'
-export const ENVIRONMENT = 'development'
+export const ENVIRONMENT = 'production'
 
 // TODO: fill in your client_id and client_secret here
 // prod
-// export const PELM_CLIENT_ID = '83af05b1-4e21-46ee-a949-16d03f38c218'
-// export const PELM_SECRET = '42e741f393d248e0fdc83a591fd85ec1aee0591ce89e9f1961698dca0b508348'
+export const PELM_CLIENT_ID = '83af05b1-4e21-46ee-a949-16d03f38c218'
+export const PELM_SECRET = '42e741f393d248e0fdc83a591fd85ec1aee0591ce89e9f1961698dca0b508348'
 
 // export const CLIENT_ID = 'fb60fd94-ffe2-4af9-a54a-c474770d43f8'
 // export const CLIENT_SECRET = '4df6a05e0b48215b0855ca6920d58e36e7666d08f486591b56bd36cae014d881'
-
-// export const PELM_CLIENT_ID = 'fb63cac0-0a27-4c10-8d35-ff4423bfa8bb'
-// export const PELM_SECRET = '93f1dbaf65399a6aae9dfab082aa9d18141e862e4b06d7064333320efc96714d'
-
-export const PELM_CLIENT_ID = '41904311-1cbb-4f55-aa65-52708dbc2860'
-export const PELM_SECRET = '497ddfb9c20fe6f79b037c4aae83f09e151cd9d42a418695057a6ddc7f84660d'
 
 export const PELM_API_URL = 'https://api.pelm.com'
 // export const PELM_API_URL = 'http://127.0.0.1:5000'
